c45f8cd42b9cb419688de2c14700145aaaf0d6c6
OgataiKhan <badellino.kevin@gmail.com>
2024-05-15 14:13:52 +0200

    feat: ticket price calculator

--- src/org/fundamentals/TrainTickets.java
+++ src/org/fundamentals/TrainTickets.java
@@ -1,2 +1,36 @@
-package org.fundamentals;public class TrainTickets {
+package org.fundamentals;
+
+import java.util.Scanner;
+
+public class TrainTickets {
+    public static void main(String[] args) {
+        // Open Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        int travelDistance, passengerAge;
+        System.out.print("Travel distance (kms): ");
+        travelDistance = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Passenger Age: ");
+        passengerAge = Integer.parseInt(scanner.nextLine());
+
+        double pricePerKm = 0.21;
+        double juniorDiscount = 0.8;
+        double seniorDiscount = 0.6;
+        double basePrice = pricePerKm * travelDistance;
+
+        // Apply discount if applicable
+        double finalPrice = basePrice;
+        if (passengerAge < 18){
+            finalPrice = basePrice * juniorDiscount;
+        } else if (passengerAge >= 65) {
+            finalPrice = basePrice * seniorDiscount;
+        }
+
+        // Print ticket price
+        System.out.println(finalPrice);
+
+        // Close Scanner
+        scanner.close();
+    }
 }
