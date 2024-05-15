849232ed82cc5606126457edc4401bc012a4f4ba
OgataiKhan <badellino.kevin@gmail.com>
2024-05-15 15:00:21 +0200

    feat: met gala

--- src/org/fundamentals/MetGala.java
+++ src/org/fundamentals/MetGala.java
@@ -1,2 +1,28 @@
-package org.fundamentals;public class MetGala {
+package org.fundamentals;
+
+import java.util.Scanner;
+
+public class MetGala {
+    public static void main(String[] args) {
+        // Open Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        String[] invitees = {"Jennifer Lopez", "Zendaya", "Chris Hemsworth", "Bad Bunny", "Cara Delevingne", "Kendall Jenner", "Uma Thurman", "Damiano David", "Luca Guadagnino"};
+
+        String user;
+        System.out.print("What is your name? ");
+        user = scanner.nextLine();
+
+        for (String invitee : invitees) {
+            if (user.equals(invitee)) {
+                System.out.println("You are on the list. Welcome to the Met Gala!");
+                return;
+            }
+        }
+
+        System.out.println("I am sorry, but you are not on the list.");
+
+        // Close Scanner
+        scanner.close();
+    }
 }
