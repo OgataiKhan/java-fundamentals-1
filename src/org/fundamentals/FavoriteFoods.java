7813147c747496735f7fe2aa55ca6675b934258f
OgataiKhan <badellino.kevin@gmail.com>
2024-05-15 14:27:57 +0200

    feat: favorite foods array

--- src/org/fundamentals/FavoriteFoods.java
+++ src/org/fundamentals/FavoriteFoods.java
@@ -1,2 +1,22 @@
-package org.fundamentals;public class FavoriteFoods {
+package org.fundamentals;
+
+public class FavoriteFoods {
+    public static void main(String[] args) {
+        String[] favoriteFoods = {"Pizza", "Burger", "Avocado", "Pesto", "Sushi", "Curry"};
+
+        System.out.print("Array length: ");
+        System.out.println(favoriteFoods.length);
+
+        System.out.print("Top favorite food: ");
+        System.out.println(favoriteFoods[0]);
+
+        System.out.print("Last favorite food: ");
+        System.out.println(favoriteFoods[favoriteFoods.length - 1]);
+
+        // Prints second middle element for even arrays
+        int middleIndex = favoriteFoods.length / 2;
+        System.out.print("Middle favorite food: ");
+        System.out.println(favoriteFoods[middleIndex]);
+
+    }
 }
